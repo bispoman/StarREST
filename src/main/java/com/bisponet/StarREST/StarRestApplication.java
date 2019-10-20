dd634cc5767b0216e1d57eabea46b3c60a589014
bispoman <daniel@bisponet.com>
2019-10-20 14:57:33 -0300

    Inicializando o projeto

---
+++ src/main/java/com/bisponet/StarREST/StarRestApplication.java
@@ -0,0 +1,13 @@
+package com.bisponet.StarREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bisponet/StarREST/StarRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bisponet.StarREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
