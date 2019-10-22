c75ea6f1501f88b48ed2bdeb50dbd98d78ea761d
bispoman <daniel@bisponet.com>
2019-10-22 01:39:51 -0300

    Optimizing imports

--- src/main/java/com/bisponet/StarREST/service/PlanetService.java
+++ src/main/java/com/bisponet/StarREST/service/PlanetService.java
@@ -5,7 +5,6 @@ import com.bisponet.StarREST.repository.PlanetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
